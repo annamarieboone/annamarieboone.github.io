d43d19dafa745d08ad2c5c25d925dc2ed905989f
anna boone <anna.boone@startribune.com>
2020-07-01 18:12:49 -0500

    change type

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+// Gulpfile
+
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var sass = require('gulp-sass');
+const fs = require('fs');
+
+gulp.task('name', function() {
+	gulp.src('index.html')
+  	.pipe(gulp.dest('assets'))
+});
+
+gulp.task('log', function() {
+  gutil.log('== My Log Task ==')
+});
+
+gulp.task('sass', function() {
+  gulp.src('styles/main.scss')
+  .pipe(sass({style: 'expanded'}))
+    .on('error', gutil.log)
+  .pipe(gulp.dest('assets'))
+});
